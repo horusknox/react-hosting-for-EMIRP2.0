22e77cc1901d6f9d409b5cdac2a9c1184cd24c63
Horusknox <shoukathali271004@gmail.com>
2025-02-21 20:55:58 +0530

    Second deployment

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     THis is the test deployment that I am doing from the class on 21/2/2025
     </div>
   );
 }
